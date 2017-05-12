84410b193b53f97afac09d4f544291401b56a5f9
Michal Hocko <mhocko@suse.com>
2017-05-12 15:46:41 -0700

    BACKPORT: mm, vmalloc: fix vmalloc users tracking properly
    Commit 1f5307b1e094 ("mm, vmalloc: properly track vmalloc users") has
    pulled asm/pgtable.h include dependency to linux/vmalloc.h and that
    turned out to be a bad idea for some architectures.  E.g.  m68k fails
    with
    
       In file included from arch/m68k/include/asm/pgtable_mm.h:145:0,
                        from arch/m68k/include/asm/pgtable.h:4,
                        from include/linux/vmalloc.h:9,
                        from arch/m68k/kernel/module.c:9:
       arch/m68k/include/asm/mcf_pgtable.h: In function 'nocache_page':
    >> arch/m68k/include/asm/mcf_pgtable.h:339:43: error: 'init_mm' undeclared (first use in this function)
        #define pgd_offset_k(address) pgd_offset(&init_mm, address)
    
    as spotted by kernel build bot. nios2 fails for other reason
    
      In file included from include/asm-generic/io.h:767:0,
                       from arch/nios2/include/asm/io.h:61,
                       from include/linux/io.h:25,
                       from arch/nios2/include/asm/pgtable.h:18,
                       from include/linux/mm.h:70,
                       from include/linux/pid_namespace.h:6,
                       from include/linux/ptrace.h:9,
                       from arch/nios2/include/uapi/asm/elf.h:23,
                       from arch/nios2/include/asm/elf.h:22,
                       from include/linux/elf.h:4,
                       from include/linux/module.h:15,
                       from init/main.c:16:
      include/linux/vmalloc.h: In function '__vmalloc_node_flags':
      include/linux/vmalloc.h:99:40: error: 'PAGE_KERNEL' undeclared (first use in this function); did you mean 'GFP_KERNEL'?
    
    which is due to the newly added #include <asm/pgtable.h>, which on nios2
    includes <linux/io.h> and thus <asm/io.h> and <asm-generic/io.h> which
    again includes <linux/vmalloc.h>.
    
    Tweaking that around just turns out a bigger headache than necessary.
    This patch reverts 1f5307b1e094 and reimplements the original fix in a
    different way.  __vmalloc_node_flags can stay static inline which will
    cover vmalloc* functions.  We only have one external user
    (kvmalloc_node) and we can export __vmalloc_node_flags_caller and
    provide the caller directly.  This is much simpler and it doesn't really
    need any games with header files.
    
    [akpm@linux-foundation.org: coding-style fixes]
    [mhocko@kernel.org: revert old comment]
      Link: http://lkml.kernel.org/r/20170509211054.GB16325@dhcp22.suse.cz
    Fixes: 1f5307b1e094 ("mm, vmalloc: properly track vmalloc users")
    Link: http://lkml.kernel.org/r/20170509153702.GR6481@dhcp22.suse.cz
    Signed-off-by: Michal Hocko <mhocko@suse.com>
    Cc: Tobias Klauser <tklauser@distanz.ch>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 8594a21cf7a8318baedbedc3fcd2967a17ddeec0)
    
    BUG=chromium:721786
    TEST=compile
    
    Change-Id: Ic7486d53e101995f71b5fc6e43bb588d512610aa
    Signed-off-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/512423
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    (cherry picked from commit bf63686b65c825db6c923b887b6a8bbfa95854fd)
    
     Conflicts:
    	mm/util.c
    
    BUG=chromium:729038
    TEST=compile
    
    Change-Id: Ic7486d53e101995f71b5fc6e43bb588d512610aa
    Reviewed-on: https://chromium-review.googlesource.com/606755
    Commit-Ready: Tomasz Figa <tfiga@chromium.org>
    Tested-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- mm/util.c
+++ mm/util.c
@@ -356,7 +356,8 @@ void *kvmalloc_node(size_t size, gfp_t flags, int node)
 	if (ret || size <= PAGE_SIZE)
 		return ret;
 
-	return __vmalloc_node_flags(size, node, flags | __GFP_HIGHMEM);
+	return __vmalloc_node_flags_caller(size, node, flags | __GFP_HIGHMEM,
+			__builtin_return_address(0));
 }
 EXPORT_SYMBOL(kvmalloc_node);
 
