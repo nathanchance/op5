b169e58f703748a65126d2227635837fac88ad56
Nathan Chancellor <natechancellor@gmail.com>
2017-09-01 21:22:37 -0700

    Revert "arm64: hw_breakpoint: fix watchpoint matching for tagged pointers"
    This reverts commit adbaaf414ae178cf5b895fb12651205bb7ca6911.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- arch/arm64/kernel/hw_breakpoint.c
+++ arch/arm64/kernel/hw_breakpoint.c
@@ -35,7 +35,6 @@
 #include <asm/traps.h>
 #include <asm/cputype.h>
 #include <asm/system_misc.h>
-#include <asm/uaccess.h>
 
 /* Breakpoint currently in use for each BRP. */
 static DEFINE_PER_CPU(struct perf_event *, bp_on_reg[ARM_MAX_BRP]);
@@ -691,7 +690,7 @@ static int watchpoint_handler(unsigned long addr, unsigned int esr,
 
 		/* Check if the watchpoint value matches. */
 		val = read_wb_reg(AARCH64_DBG_REG_WVR, i);
-		if (val != (untagged_addr(addr) & ~alignment_mask))
+		if (val != (addr & ~alignment_mask))
 			goto unlock;
 
 		/* Possible match, check the byte address select to confirm. */
