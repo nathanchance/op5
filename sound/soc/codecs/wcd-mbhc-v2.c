48e88ea632c8134a371e6766443c7e0bdde1149a
Sultanxda <sultanxda@gmail.com>
2017-12-29 12:21:16 -0800

    ASoC: wcd-mbhc: Improve detection speed and accuracy
    This is a squash of the following commits originally for msm8996:
    
    commit 10f18edf71664ff96130b4a1e22fd9ab6a1ba652
    Author: Sultanxda <sultanxda@gmail.com>
    Date:   Mon Sep 5 12:34:50 2016 -0700
    
        ASoC: wcd-mbhc: Clean up plug correction
    
        *No need to disable mic bias
        *Remove unnecessary changes
        *Add back quick detection before entering the plug-correction loop
    
        Signed-off-by: Sultanxda <sultanxda@gmail.com>
    
    commit 2babe1396a37c688f5fdbc6c7488661bea7fb90a
    Author: Sultanxda <sultanxda@gmail.com>
    Date:   Fri Dec 29 12:18:10 2017 -0800
    
        ASoC: wcd-mbhc: Fix headphone detection
    
        Currently, headphones are detected as a headset, which causes a problem
        when the user wants to use a microphone. In this situation, the on-board
        microphone is erroneously disabled and the system attempts to enable the
        microphone on the attached peripheral, even though there isn't any
        microphone on a regular pair of headphones, causing no microphone to be
        available for input.
    
        Clean up the button-interrupt disable logic to prevent interference with
        plug correction, and fix the plug correction logic so that headphones are
        detected properly.
    
        Signed-off-by: Sultanxda <sultanxda@gmail.com>
    
    commit 3b616f741b794440ef3d1bd4683bd445b9c4be4f
    Author: Sultanxda <sultanxda@gmail.com>
    Date:   Sat Aug 13 16:15:30 2016 -0700
    
        ASoC: wcd-mbhc: Ignore button interrupts until detection is complete
    
        When a plug is inserted into the jack quickly, spurious button
        interrupts are generated and interfere with UX for a short amount of
        time after plug detection is complete. To reproduce: play music out of
        speaker, plug in jack during music playback, and the music playback will
        pause after being routed to the peripheral inserted into the jack (due
        to a spurious button press).
    
        To fix this, ignore all button interrupts during plug detection, as well
        as for 750 ms after plug detection is completed.
    
        Signed-off-by: Sultanxda <sultanxda@gmail.com>
    
    commit 9085ba67246ffcd3600b47dc5cc31d3269e3d32a
    Author: Sultanxda <sultanxda@gmail.com>
    Date:   Sat Aug 13 16:04:55 2016 -0700
    
        ASoC: wcd-mbhc: Fix incorrect detection during slow plug insertion
    
        When any plug is inserted into our jack very slowly, it can be detected as
        a plug with the ground and mic pins swapped, causing MBHC to give up early
        and skew the plug detection. Instead of giving up when this happens, keep
        iterating to make sure the plug is properly detected.
    
        Signed-off-by: Sultanxda <sultanxda@gmail.com>
    
    commit c2aff3bfec79d8ab05e4f7f43d634d3d9faf6198
    Author: Sultanxda <sultanxda@gmail.com>
    Date:   Sat Aug 13 16:01:51 2016 -0700
    
        ASoC: wcd-mbhc: Speed up headphone and headset detection
    
        Instead of wasting time iterating for the full 3-second loop duration,
        exit early if the detected plug type has been determined to be a headset
        or headphones 3 times. Also speed up headset detection by treating
        high-impedance headphones as a headset, as our hardware does not properly
        detect high-impedance headphones (it usually mistakes a headset for
        high-impedance headphones and significantly slows down detection times).
    
        Signed-off-by: Sultanxda <sultanxda@gmail.com>
    
    commit 742cfb13b79ad64f884147ae3340870e9a9bdc21
    Author: Sultanxda <sultanxda@gmail.com>
    Date:   Sat Aug 13 15:58:06 2016 -0700
    
        ASoC: wcd-mbhc: Reduce latency during plug correction
    
        Sleeping for a very long time in here leads to slow detections and poor UX.
        Instead of sleeping for the same amount of time on each iteration, use a
        dynamic delay so that the delay is extended when the number of iterations
        goes up. Also don't double check if a stop request has been issued (waste
        of time).
    
        Signed-off-by: Sultanxda <sultanxda@gmail.com>
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -1201,6 +1201,21 @@ exit:
 	return spl_hs;
 }
 
+#if 1
+static void wcd_headset_btn_delay(struct work_struct *work)
+{
+	struct wcd_mbhc *mbhc =
+		container_of(work, typeof(*mbhc), mbhc_btn_delay_dwork.work);
+	/*
+	 * Allow delay between detection completion and the time when
+	 * headset button presses are allowed to be processed. This
+	 * is done in order to prevent spurious button interrupts
+	 * right after plug detection is finished.
+	 */
+	mbhc->ignore_btn_intr = false;
+}
+#endif
+
 static void wcd_correct_swch_plug(struct work_struct *work)
 {
 	struct wcd_mbhc *mbhc;
@@ -1218,11 +1233,20 @@ static void wcd_correct_swch_plug(struct work_struct *work)
 	int rc, spl_hs_count = 0;
 	int cross_conn;
 	int try = 0;
+#if 1
+	int retry = 0;
+	int headset_cnt = 0;
+#endif
 
 	pr_debug("%s: enter\n", __func__);
 
 	mbhc = container_of(work, struct wcd_mbhc, correct_plug_swch);
 	codec = mbhc->codec;
+
+#if 1
+	cancel_delayed_work_sync(&mbhc->mbhc_btn_delay_dwork);
+	mbhc->ignore_btn_intr = true;
+#endif
 
 	/*
 	 * Enable micbias/pullup for detection in correct work.
@@ -1294,6 +1318,9 @@ correct_plug_type:
 
 	timeout = jiffies + msecs_to_jiffies(HS_DETECT_PLUG_TIME_MS);
 	while (!time_after(jiffies, timeout)) {
+#if 1
+		retry++;
+#endif
 		if (mbhc->hs_detect_work_stop) {
 			pr_debug("%s: stop requested: %d\n", __func__,
 					mbhc->hs_detect_work_stop);
@@ -1346,7 +1373,11 @@ correct_plug_type:
 		 * instead of hogging system by contineous polling, wait for
 		 * sometime and re-check stop request again.
 		 */
+#if 1
+		msleep(5 * retry);
+#else
 		msleep(180);
+#endif
 		if (hs_comp_res && (spl_hs_count < WCD_MBHC_SPL_HS_CNT)) {
 			spl_hs = wcd_mbhc_check_for_spl_headset(mbhc,
 								&spl_hs_count);
@@ -1358,6 +1389,19 @@ correct_plug_type:
 			}
 		}
 
+#if 1
+		/*
+		 * It's pretty certain to be a headset after being detected
+		 * as such 10 times, so exit early to reduce detection
+		 * latency.
+		 */
+		if (plug_type == MBHC_PLUG_TYPE_HEADSET) {
+			if (++headset_cnt == 10) {
+				wrk_complete = false;
+				break;
+			}
+		}
+#endif
 		if ((!hs_comp_res) && (!is_pa_on)) {
 			/* Check for cross connection*/
 			ret = wcd_check_cross_conn(mbhc);
@@ -1379,7 +1423,12 @@ correct_plug_type:
 					pr_err("%s: switch didn't work\n",
 						  __func__);
 					plug_type = MBHC_PLUG_TYPE_GND_MIC_SWAP;
+#if 1
+					/* Retry instead in case of a noisy detection */
+					continue;
+#else
 					goto report;
+#endif
 				} else {
 					plug_type = MBHC_PLUG_TYPE_GND_MIC_SWAP;
 				}
@@ -1562,6 +1611,10 @@ exit:
 		mbhc->mbhc_cb->hph_pull_down_ctrl(codec, true);
 
 	mbhc->mbhc_cb->lock_sleep(mbhc, false);
+#if 1
+	schedule_delayed_work(&mbhc->mbhc_btn_delay_dwork,
+					msecs_to_jiffies(750));
+#endif
 	pr_debug("%s: leave\n", __func__);
 }
 
@@ -2061,6 +2114,11 @@ static irqreturn_t wcd_mbhc_btn_press_handler(int irq, void *data)
 				__func__);
 		goto done;
 	}
+#if 1
+	/* Don't process button interrupts immediately after plug detection */
+	if (mbhc->ignore_btn_intr)
+		goto done;
+#endif
 	mbhc->buttons_pressed |= mask;
 
 /*zhiguang.su@MultiMedia.AudioDrv , 2016/5/24, add for debug*/
@@ -2113,6 +2171,14 @@ static irqreturn_t wcd_mbhc_release_handler(int irq, void *data)
 		goto exit;
 
 	}
+#if 1
+	/* Don't process button interrupts immediately after plug detection */
+	if (mbhc->ignore_btn_intr) {
+		wcd_cancel_btn_work(mbhc);
+		mbhc->buttons_pressed &= ~WCD_MBHC_JACK_BUTTON_MASK;
+		goto exit;
+	}
+#endif
 	if (mbhc->buttons_pressed & WCD_MBHC_JACK_BUTTON_MASK) {
 
     /*zhiguang.su@MultiMedia.AudioDrv , 2016/5/24, add for debug*/
@@ -2931,6 +2997,10 @@ int wcd_mbhc_init(struct wcd_mbhc *mbhc, struct snd_soc_codec *codec,
 		INIT_DELAYED_WORK(&mbhc->mbhc_firmware_dwork,
 				  wcd_mbhc_fw_read);
 		INIT_DELAYED_WORK(&mbhc->mbhc_btn_dwork, wcd_btn_lpress_fn);
+#if 1
+		INIT_DELAYED_WORK(&mbhc->mbhc_btn_delay_dwork,
+						wcd_headset_btn_delay);
+#endif
 	}
 	mutex_init(&mbhc->hphl_pa_lock);
 	mutex_init(&mbhc->hphr_pa_lock);
