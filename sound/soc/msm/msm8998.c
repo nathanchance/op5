5d939c4877667f47e21c05aeef1967015ee27d77
Nathan Chancellor <natechancellor@gmail.com>
2018-01-02 23:45:17 -0700

    sound: Add back OnePlus's wsa hack
    OnePlus 5T has a complete fit without this.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- sound/soc/msm/msm8998.c
+++ sound/soc/msm/msm8998.c
@@ -7164,6 +7164,8 @@ static int msm_init_wsa_dev(struct platform_device *pdev,
 	int found = 0;
 	int ret = 0;
 
+	return ret;
+
 	/* Get maximum WSA device count for this platform */
 	ret = of_property_read_u32(pdev->dev.of_node,
 				   "qcom,wsa-max-devs", &wsa_max_devs);
