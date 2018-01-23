d2bda474c1d3292112004b61453080165f0cc526
Guenter Roeck <groeck@chromium.org>
2018-01-23 13:04:00 -0800

    FIXUP: CHROMIUM: [media] v4l2-core: Use kvmalloc() for potentially big allocations
    0day reports:
    
    drivers/media/v4l2-core/v4l2-ctrls.c:
    	In function 'v4l2_ctrl_handler_init_class':
    >> drivers/media/v4l2-core/v4l2-ctrls.c:1804:17: error:
    	implicit declaration of function 'kvmalloc_array'; did you mean 'kmalloc_array'?
            hdl->buckets = kvmalloc_array(hdl->nr_of_buckets,
    	               ^~~~~~~~~~~~~~
    
    and many other related errors.
    
    Add the missing include file to fix the problem.
    
    BUG=chromium:729038
    TEST=compile
    
    Change-Id: I84b88750196c8929d1afc2891a81bbc767f1dd31
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/882001
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/media/v4l2-core/v4l2-ctrls.c
+++ drivers/media/v4l2-core/v4l2-ctrls.c
@@ -21,6 +21,7 @@
 #include <linux/ctype.h>
 #include <linux/slab.h>
 #include <linux/export.h>
+#include <linux/mm.h>
 #include <media/v4l2-ioctl.h>
 #include <media/v4l2-device.h>
 #include <media/v4l2-ctrls.h>
