nathanchance/op5

commit b00bc6eb55849374d4625775c0c27e19d45a19a8

usb: f_gsi: Realign to oneplus/QC8998_O_8.0
When OnePlus updated their tree with Qualcomm patches in b3407208528d,
they left this statement out of their application of e898a0ced40c
("usb: gadget: Update default class subclass and protocol for rndis").

Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

drivers/usb/gadget/function/f_gsi.c
@@ -3188,10 +3188,6 @@ static int gsi_set_inst_name(struct usb_function_instance *fi,
 	}
 	mutex_unlock(&inst_status[prot_id].gsi_lock);
 
-	if (prot_id == IPA_USB_RNDIS)
-		config_group_init_type_name(&opts->func_inst.group, "",
-					    &gsi_func_rndis_type);
-
 	gsi = gsi_function_init(prot_id);
 	if (IS_ERR(gsi))
 		return PTR_ERR(gsi);

commit 9c4eea2ea99740522202da43a87608af1b9fe867

usb: f_gsi: Fix unused variable warning
Fixes: b00bc6eb5584 ("usb: f_gsi: Realign to oneplus/QC8998_O_8.0")
Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

drivers/usb/gadget/function/f_gsi.c
@@ -3102,6 +3102,7 @@ static ssize_t gsi_info_show(struct config_item *item, char *page)
 
 CONFIGFS_ATTR_RO(gsi_, info);
 
+#if 0
 static ssize_t gsi_rndis_wceis_show(struct config_item *item, char *page)
 {
 	struct f_gsi *gsi = to_gsi_opts(item)->gsi;
@@ -3136,6 +3137,7 @@ static struct config_item_type gsi_func_rndis_type = {
 	.ct_attrs	= gsi_rndis_attrs,
 	.ct_owner	= THIS_MODULE,
 };
+#endif
 
 
 static struct configfs_attribute *gsi_attrs[] = {