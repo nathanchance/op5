OnePlusOSS/android_kernel_oneplus_msm8998

commit e4e44606443a1d7dfa45799f9170ee493ff03dab

Improve headset compatibility.
Add related settings.

Change-Id: I23723e0c97d5f1fc9c8ab273d7d7c4201fb19354

sound/soc/codecs/wcd-mbhc-v2.c
@@ -56,6 +56,7 @@
 #define ANC_DETECT_RETRY_CNT 7
 #define WCD_MBHC_SPL_HS_CNT  1
 
+
 static int det_extn_cable_en;
 module_param(det_extn_cable_en, int,
 		S_IRUGO | S_IWUSR | S_IWGRP);
@@ -246,6 +247,7 @@ static int wcd_event_notify(struct notifier_block *self, unsigned long val,
 	struct snd_soc_codec *codec = mbhc->codec;
 	bool micbias2 = false;
 	bool micbias1 = false;
+	u8 fsm_en = 0;
 
 	pr_debug("%s: event %s (%d)\n", __func__,
 		 wcd_mbhc_get_event_string(event), event);
@@ -286,7 +288,12 @@ static int wcd_event_notify(struct notifier_block *self, unsigned long val,
 					false);
 out_micb_en:
 		/* Disable current source if micbias enabled */
-		if (!mbhc->mbhc_cb->mbhc_micbias_control) {
+		if (mbhc->mbhc_cb->mbhc_micbias_control) {
+			WCD_MBHC_REG_READ(WCD_MBHC_FSM_EN, fsm_en);
+			if (fsm_en)
+				WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_BTN_ISRC_CTL,
+						0);
+		} else {
 			mbhc->is_hs_recording = true;
 			wcd_enable_curr_micbias(mbhc, WCD_MBHC_EN_MB);
 		}
@@ -295,7 +302,18 @@ out_micb_en:
 			mbhc->mbhc_cb->set_cap_mode(codec, micbias1, true);
 		break;
 	case WCD_EVENT_PRE_MICBIAS_2_OFF:
-
+		/*
+		 * Before MICBIAS_2 is turned off, if FSM is enabled,
+		 * make sure current source is enabled so as to detect
+		 * button press/release events
+		 */
+		if (mbhc->mbhc_cb->mbhc_micbias_control &&
+		    !mbhc->micbias_enable) {
+			WCD_MBHC_REG_READ(WCD_MBHC_FSM_EN, fsm_en);
+			if (fsm_en)
+				WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_BTN_ISRC_CTL,
+							 3);
+		}
 		break;
 	/* MICBIAS usage change */
 	case WCD_EVENT_POST_DAPM_MICBIAS_2_OFF:
@@ -775,8 +793,6 @@ static void wcd_mbhc_report_plug(struct wcd_mbhc *mbhc, int insertion,
 		pr_err("%s: Reporting insertion %d(%x)\n", __func__,
 		jack_type, mbhc->hph_status);
 
-		pr_debug("%s: Reporting insertion %d(%x)\n", __func__,
-			 jack_type, mbhc->hph_status);
 		wcd_mbhc_jack_report(mbhc, &mbhc->headset_jack,
 				    (mbhc->hph_status | SND_JACK_MECHANICAL),
 				    WCD_MBHC_JACK_MASK);
@@ -923,9 +939,6 @@ static void wcd_mbhc_find_plug_and_report(struct wcd_mbhc *mbhc,
 			/* Disable HW FSM and current source */
 			WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_FSM_EN, 0);
 			WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_BTN_ISRC_CTL, 0);
-			mbhc->mbhc_cb->mbhc_micbias_control(mbhc->codec,
-				MIC_BIAS_2,
-				MICB_PULLUP_DISABLE);
 			/* Setup for insertion detection */
 			WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_ELECT_DETECTION_TYPE,
 						 1);
@@ -1094,16 +1107,18 @@ static bool wcd_is_special_headset(struct wcd_mbhc *mbhc)
 static void wcd_mbhc_update_fsm_source(struct wcd_mbhc *mbhc,
 				       enum wcd_mbhc_plug_type plug_type)
 {
+	bool micbias2;
 
+	micbias2 = mbhc->mbhc_cb->micbias_enable_status(mbhc,
+			MIC_BIAS_2);
 	switch (plug_type) {
 	case MBHC_PLUG_TYPE_HEADPHONE:
 		WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_BTN_ISRC_CTL, 3);
 		break;
 	case MBHC_PLUG_TYPE_HEADSET:
 	case MBHC_PLUG_TYPE_ANC_HEADPHONE:
-		WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_BTN_ISRC_CTL, 0);
-		mbhc->mbhc_cb->mbhc_micbias_control(mbhc->codec, MIC_BIAS_2,
-						MICB_PULLUP_ENABLE);
+		if (!mbhc->is_hs_recording && !micbias2)
+			WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_BTN_ISRC_CTL, 3);
 		break;
 	default:
 		WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_BTN_ISRC_CTL, 0);
@@ -1779,7 +1794,6 @@ static int wcd_mbhc_get_button_mask(struct wcd_mbhc *mbhc)
 
 	return mask;
 }
-
 static irqreturn_t wcd_mbhc_hs_ins_irq(int irq, void *data)
 {
 	struct wcd_mbhc *mbhc = data;
@@ -1861,7 +1875,11 @@ determine_plug:
 	pr_debug("%s: leave\n", __func__);
 	return IRQ_HANDLED;
 }
-
+#ifndef VENDOR_EDIT
+/*
+ * liuhaituo@MultiMedia 2018/4/26 disable Elect Remove irq to resolve
+ * apple-headset button issue
+ */
 static irqreturn_t wcd_mbhc_hs_rem_irq(int irq, void *data)
 {
 	struct wcd_mbhc *mbhc = data;
@@ -1906,6 +1924,8 @@ static irqreturn_t wcd_mbhc_hs_rem_irq(int irq, void *data)
 	WCD_MBHC_REG_READ(WCD_MBHC_MIC_SCHMT_RESULT, mic_sch);
 	WCD_MBHC_REG_READ(WCD_MBHC_HS_COMP_RESULT, hs_comp_result);
 
+	pr_debug("%s: hphl_sch %#x, mic_sch %#x, hs_comp_result %#x\n",
+			__func__, hphl_sch, mic_sch, hs_comp_result);
 	if (removed) {
 		if (!(hphl_sch && mic_sch && hs_comp_result)) {
 			/*
@@ -1976,7 +1996,7 @@ report_unplug:
 	pr_debug("%s: leave\n", __func__);
 	return IRQ_HANDLED;
 }
-
+#endif
 static void wcd_btn_lpress_fn(struct work_struct *work)
 {
 	struct delayed_work *dwork;
@@ -2994,6 +3014,11 @@ int wcd_mbhc_init(struct wcd_mbhc *mbhc, struct snd_soc_codec *codec,
 				   false);
 	clear_bit(WCD_MBHC_ELEC_HS_INS, &mbhc->intr_status);
 
+#ifndef VENDOR_EDIT
+/*
+ * liuhaituo@MultiMedia 2018/4/26 disable Elect Remove irq to resolve
+ * apple-headset button issue
+ */
 	ret = mbhc->mbhc_cb->request_irq(codec,
 					 mbhc->intr_ids->mbhc_hs_rem_intr,
 					 wcd_mbhc_hs_rem_irq,
@@ -3005,6 +3030,7 @@ int wcd_mbhc_init(struct wcd_mbhc *mbhc, struct snd_soc_codec *codec,
 	}
 	mbhc->mbhc_cb->irq_control(codec, mbhc->intr_ids->mbhc_hs_rem_intr,
 				   false);
+#endif
 	clear_bit(WCD_MBHC_ELEC_HS_REM, &mbhc->intr_status);
 
 	ret = mbhc->mbhc_cb->request_irq(codec, mbhc->intr_ids->hph_left_ocp,
@@ -3031,8 +3057,14 @@ int wcd_mbhc_init(struct wcd_mbhc *mbhc, struct snd_soc_codec *codec,
 err_hphr_ocp_irq:
 	mbhc->mbhc_cb->free_irq(codec, mbhc->intr_ids->hph_left_ocp, mbhc);
 err_hphl_ocp_irq:
+#ifndef VENDOR_EDIT
+/*
+ * liuhaituo@MultiMedia 2018/4/26 disable Elect Remove irq to resolve
+ * apple-headset button issue
+ */
 	mbhc->mbhc_cb->free_irq(codec, mbhc->intr_ids->mbhc_hs_rem_intr, mbhc);
 err_mbhc_hs_rem_irq:
+#endif
 	mbhc->mbhc_cb->free_irq(codec, mbhc->intr_ids->mbhc_hs_ins_intr, mbhc);
 err_mbhc_hs_ins_irq:
 	mbhc->mbhc_cb->free_irq(codec, mbhc->intr_ids->mbhc_btn_release_intr,
@@ -3062,7 +3094,13 @@ void wcd_mbhc_deinit(struct wcd_mbhc *mbhc)
 	mbhc->mbhc_cb->free_irq(codec, mbhc->intr_ids->mbhc_btn_release_intr,
 				mbhc);
 	mbhc->mbhc_cb->free_irq(codec, mbhc->intr_ids->mbhc_hs_ins_intr, mbhc);
+#ifndef VENDOR_EDIT
+/*
+ * liuhaituo@MultiMedia 2018/4/26 disable Elect Remove irq to resolve
+ * apple-headset button issue
+ */
 	mbhc->mbhc_cb->free_irq(codec, mbhc->intr_ids->mbhc_hs_rem_intr, mbhc);
+#endif
 	mbhc->mbhc_cb->free_irq(codec, mbhc->intr_ids->hph_left_ocp, mbhc);
 	mbhc->mbhc_cb->free_irq(codec, mbhc->intr_ids->hph_right_ocp, mbhc);
 	if (mbhc->mbhc_cb && mbhc->mbhc_cb->register_notifier)

arch/arm/boot/dts/qcom/cheeseburger.dtsi
@@ -726,6 +726,18 @@
 &snd_9335 {
 	qcom,mbhc-audio-jack-type = "4-pole-jack";
 };
+/*
+ * liuhaituo@MultiMediaAudio modified for adapation phone
+ * headset and AKG headset-mic 2018/4/26
+ */
+&slim_aud {
+	tasha_codec {
+		qcom,cdc-micbias1-mv = <1800>;
+		qcom,cdc-micbias2-mv = <2700>;
+		qcom,cdc-micbias3-mv = <1800>;
+		qcom,cdc-micbias4-mv = <1800>;
+	};
+};
 
 /* Audio */
 &soc {

arch/arm/boot/dts/qcom/dumpling.dtsi
@@ -749,8 +749,10 @@
 &snd_9335 {
 	qcom,mbhc-audio-jack-type = "4-pole-jack";
 };
-/* liuhaituo@MultiMediaAudio modified for adapation phone headset 2018/3/2 */
 /*
+ * liuhaituo@MultiMediaAudio modified for adapation phone
+ * headset and AKG headset-mic 2018/4/26
+ */
 &slim_aud {
 	tasha_codec {
 		qcom,cdc-micbias1-mv = <1800>;
@@ -759,7 +761,6 @@
 		qcom,cdc-micbias4-mv = <1800>;
 	};
 };
-*/
 
 /* Audio */
 &soc {

nathanchance/op5

commit d306e21901a65926bcde5840ed6e7dd200c04ec8

ASoC: wcd-mbhc: Don't rely on VENDOR_EDIT
Fixes: e4e44606443a ("Improve headset compatibility.")
Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

sound/soc/codecs/wcd-mbhc-v2.c
@@ -1928,7 +1928,7 @@ determine_plug:
 	pr_debug("%s: leave\n", __func__);
 	return IRQ_HANDLED;
 }
-#ifndef VENDOR_EDIT
+#if 0
 /*
  * liuhaituo@MultiMedia 2018/4/26 disable Elect Remove irq to resolve
  * apple-headset button issue
@@ -3084,7 +3084,7 @@ int wcd_mbhc_init(struct wcd_mbhc *mbhc, struct snd_soc_codec *codec,
 				   false);
 	clear_bit(WCD_MBHC_ELEC_HS_INS, &mbhc->intr_status);
 
-#ifndef VENDOR_EDIT
+#if 0
 /*
  * liuhaituo@MultiMedia 2018/4/26 disable Elect Remove irq to resolve
  * apple-headset button issue
@@ -3127,7 +3127,7 @@ int wcd_mbhc_init(struct wcd_mbhc *mbhc, struct snd_soc_codec *codec,
 err_hphr_ocp_irq:
 	mbhc->mbhc_cb->free_irq(codec, mbhc->intr_ids->hph_left_ocp, mbhc);
 err_hphl_ocp_irq:
-#ifndef VENDOR_EDIT
+#if 0
 /*
  * liuhaituo@MultiMedia 2018/4/26 disable Elect Remove irq to resolve
  * apple-headset button issue
@@ -3164,7 +3164,7 @@ void wcd_mbhc_deinit(struct wcd_mbhc *mbhc)
 	mbhc->mbhc_cb->free_irq(codec, mbhc->intr_ids->mbhc_btn_release_intr,
 				mbhc);
 	mbhc->mbhc_cb->free_irq(codec, mbhc->intr_ids->mbhc_hs_ins_intr, mbhc);
-#ifndef VENDOR_EDIT
+#if 0
 /*
  * liuhaituo@MultiMedia 2018/4/26 disable Elect Remove irq to resolve
  * apple-headset button issue